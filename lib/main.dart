96413f382365df6c3df5d803eaca401d1ea0d7bf
JStuve <stuve.josh@gmail.com>
2019-01-21 14:20:16 -0600

    Finished demo 2. Seperated widgets on files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
+import 'RandomWords_Page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,51 +10,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Gen',
-      home: RandomWords()
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Gen')
+      theme: new ThemeData(
+        primaryColor: Colors.white
       ),
-      body: buildSuggestions()
-    );
-  }
-
-  Widget buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-
-        final index = i ~/2;
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return buildRow(_suggestions[index]);
-      });  
-  }
-
-  Widget buildRow(WordPair pair){
-    return ListTile(
-      title: Text (
-        pair.asPascalCase,
-        style: _biggerFont
-      )
+      home: new RandomWords()
     );
   }
 }
+
