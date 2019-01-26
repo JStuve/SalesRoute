048c517821c1680ef8b900c46e20cc7099c7aaa0
JStuve <stuve.josh@gmail.com>
2019-01-26 14:51:02 -0600

    Clients are now listing based on the json file

--- lib/service/Client_Get.dart
+++ lib/service/Client_Get.dart
@@ -1,10 +1,5 @@
-
 import 'dart:async' show Future;
-import 'dart:convert';
-
 import 'package:flutter/services.dart' show rootBundle;
-
-import '../data/Client.dart';
 
 Future<String> getClientFromJson() async{
   return await rootBundle.loadString('assets/clients.json');
--- lib/views/Client_View.dart
+++ lib/views/Client_View.dart
@@ -1,4 +1,5 @@
-import 'dart:async' show Future;
+import 'dart:convert' as JSON;
+
 import 'package:flutter/material.dart';
 
 import '../service/Client_Get.dart';
@@ -11,31 +12,45 @@ class ClientView extends StatefulWidget {
 
 class ClientViewState extends State<ClientView> {
   
-  String clients = "";
+  Map clients = {};
+  List clientList = [];
+  int clientListLength = 0;
+
   ClientViewState() {
     getClientFromJson().then((val) => setState(() {
-      clients = val;
+      clients = JSON.jsonDecode(val);
     }));
   }
 
   @override
   Widget build(BuildContext context){
     
-    print(clients);
-
-    return ListView.builder(
-      padding: EdgeInsets.all(8.0),
-      itemBuilder: (BuildContext context, int i) {
-
-        return Text('Index $i');
-      },
-    );
+    // Pre check to make sure the defualt value is Zero, not Null
+    if(clients.isNotEmpty){
+      clientList = clients['clients'];
+      clientListLength = clientList.length;
+      if (clientListLength!= null){
+        // print('TEST:  ${clientList.length}');
+        return ListView.builder(
+          padding: EdgeInsets.all(8.0),
+          itemExtent: 20.0,
+          itemCount: clientListLength,
+          itemBuilder: (BuildContext context, int i) {
+            Client c = new Client.fromJson(clientList[i]);
+            return clientRow(c);
+          },
+        );
+      }
+    }
+    else {
+      return Text('Shit...');
+    }
   }
 
   Widget clientRow(Client c){
     
     return ListTile(
-      title: Text("Test")
+      title: Text(c.clientName)
     );
   }
 
